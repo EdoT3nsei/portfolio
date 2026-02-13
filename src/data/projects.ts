ae73cc0b94b6063df9e1004c6743c01e09312a25
EdoT3nsei <emiledonzel@bluewin.ch>
2026-02-13 19:21:45 +0100

    ajout nouveaux projets

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -6,6 +6,9 @@ import cover3 from '../assets/images/terravaud_1.png';
 import cover4 from '../assets/images/empire_cover.png';
 import cover5 from '../assets/images/baleinev_2.png';
 import cover6 from '../assets/images/beer_cover.png';
+import cover7 from '../assets/images/firewatch.jpg';
+import cover8 from '../assets/images/sed_cover.png';
+import cover9 from '../assets/images/coachlab_cover.jpg';
 
 import thumbnail1 from '../assets/images/map_1-thumb.webp';
 import thumbnail2 from '../assets/images/badge_kolding_24-thumb.webp';
@@ -35,7 +38,65 @@ import photoBaleinev4 from '../assets/images/baleinev_4.png';
 //6
 import photoBeer1 from '../assets/images/beer_bottles.png';
 
+//7
+import videoFirewatch from '../assets/videos/Intro_edo.mp4';
+import behindthesceneFirewatch from '../assets/videos/plans_after_effects.mp4';
+
+//8
+import sed1 from '../assets/images/sed_1.png';
+import sed2 from '../assets/images/sed_2.png';
+import sed3 from '../assets/images/sed_3.png';
+import sed4 from '../assets/images/sed_4.png';
+import sed5 from '../assets/images/sed_5.png';
+
+//9
+import coachlab1 from '../assets/images/coachlab_1.jpeg';
+import coachlab2 from '../assets/images/coachlab_2.jpeg';
+import coachlab3 from '../assets/images/coachlab_3.png';
+import coachlab4 from '../assets/images/coachlab_4.jpeg';
+
 export const projects: Project[] = [
+    {
+    id: 8,
+    title: "Redesign of the SED's educational quizzes ",
+    category: "UX/UI design",
+    image: cover8,
+    thumbnail: cover8,
+    description: "Redesign of the SED's educational quizzes for a teenage audience as part of by Bachelor Thesis.",
+    fullDescription: "I had the opportunity to work with the Swiss Seismological Service (SED) as part of my Bachelor Thesis. The goal of the project was to rethink the actual quizzes present on the SED's website to make them more engaging for a teenage audience. I conducted user research, created wireframes and prototypes, and tested the new design with users to ensure it met their needs and expectations.",
+    client: "Swiss Seismological Service",
+    clientUrl: "https://www.seismo.ethz.ch/",
+    date: "2025",
+    tools: ["Figma", "Adobe Illustrator"],
+    gallery: [
+      sed2, sed3, sed4, sed5
+    ]    
+  },
+    {
+    id: 9,
+    title: "CoachLab",
+    category: "Communication campaign",
+    image: cover9,
+    thumbnail: cover9,
+    description: "Communication campaign for the UEFA Women's Euro 2025.",
+    fullDescription: "As part of a school project, we where mandated by Amzaon and the UEFA to create something to promote female coaches in football. We created a communication campaign called CoachLab, which notably included a companion webapp.",
+    client: "School Project - HEIG-VD / Amazon / UEFA Women's Euro 2025",
+    clientUrl: "https://pi25.heig-vd.ch/en",
+    date: "2025",
+    tools: ["Figma", "Adobe Illustrator", "Adobe Photoshop", "VueJS", "TailwindCSS"],
+    gallery: [
+      coachlab1, coachlab2, coachlab3, coachlab4
+    ],
+    collaborators: [
+      { name: "Maxime Peretti", website: "https://www.linkedin.com/in/maxime-peretti/" },
+      { name: "Léo Winterhalter", website: "https://www.linkedin.com/in/leo-winterhalter/" },
+      { name: "Nicolas Aerny", website: "https://nicolasaerny.ch/" },
+      { name: "Clément Künzi", website: "https://clement-kunzi.ch/" },
+      { name: "Leo Emeri", website: "https://www.linkedin.com/in/leo-emeri/" },
+      { name: "Loïc Monnerat", website: "https://www.linkedin.com/in/loic-monnerat/" },
+      { name: "Tristan Montanari", website: "https://www.linkedin.com/in/tristan-montanari/" }
+    ]       
+  },
   {
     id: 1,
     title: "TerraVaud",
@@ -49,9 +110,7 @@ export const projects: Project[] = [
     date: "2024",
     tools: ["Figma", "Adobe Premiere Pro", "Adobe Illustrator", "Adobe InDesign", "VueJS", "Laravel"],
     gallery: [
-      videoTerravaud,
-      photoTerravaud1,
-      cover3
+      videoTerravaud, photoTerravaud1, cover3
     ],
     collaborators: [
       { name: "Maxime Peretti", website: "https://www.linkedin.com/in/maxime-peretti/" },
@@ -126,6 +185,23 @@ export const projects: Project[] = [
       photoMap3
     ]
   },
+  {
+    id: 7,
+    title: "Intro Animation",
+    category: "Motion Design",
+    image: cover7,
+    thumbnail: cover7,
+    description: "Motion design for a video intro.",
+    fullDescription: "Motion design based on the video game Firewatch for a video intro. I used the parallax effect to create a sense of depth and movement in the animation.",
+    client: "Private project",
+    date: "2021",
+    tools: ["Adobe Photoshop", "Adobe After Effects", "Adobe Premiere Pro"],
+    gallery: [
+      videoFirewatch,
+      behindthesceneFirewatch,
+    ]    
+  },
+
   // {
   //   id: 3,
   //   title: "Kolding 2024 scouting Badge",
